b58e169eb09bc6e33f1e7dcb98a1429f3c98e098
dynablaze@gmail.com <dynablaze@gmail.com>
2024-05-24 13:56:25 +0100

    made changes to app.js by CSC 314

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE !'));
+app.get('/', (req, res) => res.send(' this is CSS 314 cloud computing DEVOPS ; CI-CD PIPELINE STEP 1 !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
