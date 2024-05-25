4e4af2ff1649c72339c0e3a08282837da24acbca
dynablaze@gmail.com <dynablaze@gmail.com>
2024-05-25 13:29:18 +0100

    this is first AWS commit on  pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is CSS 314 cloud computing DEVOPS ; CI-CD PIPELINE STEP 1 !'));
+app.get('/', (req, res) => res.send(' this is CSS 314 cloud computing DEVOPS ; CI-CD PIPELINE. THIS PAGE INDICATES SUCCESS  !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
