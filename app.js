f9ec145976dfc3fcd4dc51aae9af580f669fb3ca
wisdom914 <sandywizzy99@gmail.com>
2024-05-28 12:38:57 +0100

    added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html cc 314'));
+    res.sendFile(path.join(__dirname, 'views/index.html cc 314ff'));
 });
 
 app.get('/about', (req, res) => {
