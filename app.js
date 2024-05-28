bc3c4268623df42e99177bbf5ea1913b3ee511bb
wisdom914 <sandywizzy99@gmail.com>
2024-05-28 12:28:42 +0100

    added cc 314

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.sendFile(path.join(__dirname, 'views/index.html cc 314'));
 });
 
 app.get('/about', (req, res) => {
